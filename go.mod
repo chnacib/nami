0a67d2686aa8f92ef41e1489946b3a3565704242
Carlos Henrique <84737230+chnacib@users.noreply.github.com>
2025-05-15 19:30:56 -0300

    improve error handling in describe task api call (#15)

--- go.mod
+++ go.mod
@@ -11,7 +11,6 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/applicationautoscaling v1.21.3
 	github.com/aws/aws-sdk-go-v2/service/ecs v1.57.0
 	github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2 v1.19.14
-	github.com/schollz/progressbar/v3 v3.18.0
 	github.com/spf13/cobra v1.7.0
 )
 
@@ -26,12 +25,9 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.13 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.20.0 // indirect
 	github.com/aws/smithy-go v1.22.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/term v0.28.0 // indirect
 )
--- pkg/ecs/describetask.go
+++ pkg/ecs/describetask.go
@@ -1,48 +1,130 @@
 package ecs
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ecs"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+
+	"github.com/chnacib/nami/pkg/config"
+
 	"github.com/spf13/cobra"
 )
 
 func DescribeTask() *cobra.Command {
+
 	var cluster string
+
 	cmd := &cobra.Command{
-		Use:     "task",
+
+		Use: "task [task-id]",
+
 		Aliases: []string{"td"},
-		Short:   "Describe ECS task",
+
+		Short: "Describe ECS task",
+
+		Args: cobra.ExactArgs(1),
+
 		Run: func(cmd *cobra.Command, args []string) {
-			name := args[0]
-			sess := session.Must(session.NewSessionWithOptions(session.Options{
-				SharedConfigState: session.SharedConfigEnable,
-			}))
-			client := ecs.New(sess)
+
+			taskID := args[0]
+
+			if strings.TrimSpace(taskID) == "" {
+
+				fmt.Fprintf(os.Stderr, "Error: Task ID cannot be empty\n")
+
+				os.Exit(0)
+
+			}
+
+			cfg, err := config.LoadConfig()
+
+			if err != nil {
+
+				fmt.Errorf("failed to load configuration: %w", err)
+
+			}
+
+			client := ecs.NewFromConfig(cfg.AwsConfig)
 
 			input := &ecs.DescribeTasksInput{
-				Tasks: []*string{
-					aws.String(name),
-				},
+				Tasks:   []string{taskID},
 				Cluster: aws.String(cluster),
 			}
 
-			response, err := client.DescribeTasks(input)
+			response, err := client.DescribeTasks(context.Background(), input)
+
 			if err != nil {
-				fmt.Println(err)
+
+				handleAWSError(err, taskID, cluster)
+
 				os.Exit(0)
+
 			}
-			fmt.Println(response)
+
+			if len(response.Tasks) == 0 {
+
+				fmt.Fprintf(os.Stderr, "Error: No task with ID '%s' found in cluster '%s'\n", taskID, cluster)
+
+				os.Exit(0)
+
+			}
+
+			data, err := json.MarshalIndent(response.Tasks[0], "", "  ")
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error formatting response: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Println(string(data))
 
 		},
 	}
 
-	cmd.Flags().StringVarP(&cluster, "cluster", "c", "string", "ECS Cluster name")
+	cmd.Flags().StringVarP(&cluster, "cluster", "c", "", "ECS Cluster name (required)")
+
 	cmd.MarkFlagRequired("cluster")
 
 	return cmd
+
+}
+
+func handleAWSError(err error, taskID, clusterName string) {
+
+	if awsErr, ok := err.(awserr.Error); ok {
+
+		switch awsErr.Code() {
+
+		case "ClusterNotFoundException":
+
+			fmt.Fprintf(os.Stderr, "Error: Cluster '%s' not found\n", clusterName)
+
+		case "InvalidParameterException":
+
+			fmt.Fprintf(os.Stderr, "Error: Invalid parameter: %s\n", awsErr.Message())
+
+		case "AccessDeniedException":
+
+			fmt.Fprintf(os.Stderr, "Error: Access denied: %s\n", awsErr.Message())
+
+		default:
+
+			fmt.Fprintf(os.Stderr, "Error: AWS Error (%s): %s\n", awsErr.Code(), awsErr.Message())
+
+		}
+
+	} else {
+
+		fmt.Fprintf(os.Stderr, "Error: Failed to describe task '%s': %v\n", taskID, err)
+
+	}
 
 }
